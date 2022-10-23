ff5559e62cbafe688a7b38385221129c7f262e77
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-10-23 19:49:32 +0800

    provided those lets go

---
+++ scripts/tester-blocks.js
@@ -0,0 +1,32 @@
+//Global tester variable.
+//Used for eval calls.
+global.testers = {
+    effectTesterFx: Effect(20, e => {}),
+    error: null
+}
+
+//Effect tester.
+const effectTester = extend(MessageBlock, "effect-tester", {})
+
+//Effect tester build. Contains building code.
+effectTester.buildType = () => {
+    const build = extend(MessageBlock.MessageBuild, effectTester, {
+        
+        buildConfiguration(table){
+            this.super$buildConfiguration(table)
+            table.button(Icon.star, () => {
+                try{
+                    Vars.mods.scripts.runConsole("global.testers.effectTesterFx = new Effect(30, e => {" + this.message + "})")
+                    global.testers.effectTesterFx.at(this.x, this.y)
+                }catch(err){
+                    global.testers.error = err
+                }
+                
+                
+            })
+            
+        }
+        
+    })
+    return build
+}
