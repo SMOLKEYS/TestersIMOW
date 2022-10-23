efc4b1d902e1a5c38498c2f58548d315b67631a1
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-10-23 21:55:58 +0800

    no.

--- scripts/tester-blocks.js
+++ scripts/tester-blocks.js
@@ -18,7 +18,7 @@ effectTester.buildType = () => {
             this.super$buildConfiguration(table)
             table.button(Icon.star, Styles.cleari, () => {
                 try{
-                    Vars.mods.scripts.runConsole("global.testers.effectTesterFx = new Effect(" + this.effLength + ", e => {" + this.message + "})")
+                    eval("global.testers.effectTesterFx = new Effect(" + this.effLength + ", e => {try{" + this.message + "}catch(er){Lines.circle(e.x, e.y, e.fin(Interp.pow10Out) * 30)}})")
                     global.testers.effectTesterFx.at(this.x, this.y)
                 }catch(err){
                     global.testers.error = err
