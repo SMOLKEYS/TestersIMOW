bcfaf83a5adc5a3f80bcc234af6e5221e3d4d02f
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-10-24 14:30:09 +0800

    script tester

--- scripts/tester-blocks.js
+++ scripts/tester-blocks.js
@@ -21,7 +21,7 @@ effectTester.buildType = () => {
                     eval("global.testers.effectTesterFx = new Effect(" + this.effLength + ", e => {try{" + this.message + "}catch(er){Draw.color(Pal.remove); Lines.stroke(e.fout() * 5); Lines.circle(e.x, e.y, e.fin(Interp.pow10Out) * 30); Fonts.def.draw(er.toString(), e.x, e.y + e.fin(Interp.pow3Out) * 15, Color.white, 0.24, false, 1)}})")
                     global.testers.effectTesterFx.at(this.x, this.y)
                 }catch(err){
-                    global.testers.error = err
+                    Vars.ui.showError(err.toString())
                 }
             }).size(40)
             
@@ -42,4 +42,43 @@ effectTester.buildType = () => {
         
     })
     return build
+}
+
+
+const scriptTester = extend(MessageBlock, "script-tester", {})
+
+scriptTester.buildType = () => {
+    const build = extend(MessageBlock.MessageBuild, scriptTester, {
+        
+        strCont: "",
+        errored: false,
+        
+        buildConfiguration(table){
+            this.super$buildConfiguration(table)
+            table.button(Icon.wrench, Styles.cleari, () => {
+                try{
+                    if(this.strCont == this.message && this.errored) return
+                    this.strCont = this.message
+                    eval(this.message)
+                    this.errored = false
+                }catch(err){
+                    Vars.ui.showError(err.toString())
+                    this.errored = true
+                }
+            })
+        },
+        
+       write(write) {
+           this.super$write(write)
+           write.str(this.strCont)
+           write.b(this.errored)
+       },
+
+       read(read, rev) {
+           this.super$read(read, rev)
+           this.strCont = read.str()
+           this.errored = read.b()
+       }
+       
+    })
 }
